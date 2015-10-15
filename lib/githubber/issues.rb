68b8d19c81bd6d4d90f0bd1aecf191ed828534ea
Daniel Nicodemo <dan.nicodemo@gmail.com>
2015-10-14 21:03:32 -0400

    Fixing the internet like a champion

--- lib/githubber/issues.rb
+++ lib/githubber/issues.rb
@@ -11,17 +11,30 @@ module Githubber
 			}
 		end
 
+#add/open a new issue in the owner's repo.  title = "string
+	def new(owner, repo, title, content, assignee)
+			PullRequets.post("/repos/#{owner}/#{repo}/issues",
+				:headers => @auth,
+				:body => {"title" => title, "body" => content, "assignee" => assignee}.to_json)
+
+#comment on an existing issue
 		def comment(owner, repo, issue_num, comment)
-			PullRequests.patch("repos/#{owner}/#{repo}/issues/#{issue_num}", :headers => @auth, :body => { "body" => comment }.to_json)
+			PullRequests.patch("/repos/#{owner}/#{repo}/issues/#{issue_num}", 
+				:headers => @auth, 
+				:body => { "body" => comment }.to_json)
 		end
 
+#list issues in the owner's repo
 		def list_issues(owner, repo)
-			PullRequests.get("repos/#{owner}/#{repo}/issues", :headers => @auth)
+			PullRequests.get("/repos/#{owner}/#{repo}/issues", :headers => @auth)
 		end
 
+
+#close an already opened issue in the owner's repo MUST KNOW ISSUE NUMBER
 		def close_issue(owner, repo, issue_num)
-			PullRequests.patch("repos/#{owner}/#{repo}/issues/#{issue_num}", :headers => @auth, :body => {"state" => "closed"}.to_json)
+			PullRequests.patch("/repos/#{owner}/#{repo}/issues/#{issue_num}", 
+				:headers => @auth, 
+				:body => {"state" => "closed"}.to_json)
 		end
 	end
 end
-
